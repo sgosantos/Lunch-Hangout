ed5723fdd59f69a89d7c0c23e395f5c3977efb80
Igor Oliveira <igor.oliveira@liferay.com>
2016-08-19 14:44:50 -0300

    main activity

--- app/src/main/java/com/liferay/hackaday/lunchhangout/MainActivity.java
+++ app/src/main/java/com/liferay/hackaday/lunchhangout/MainActivity.java
@@ -1,15 +1,28 @@
 package com.liferay.hackaday.lunchhangout;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.liferay.hackaday.lunchhangout.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	private ActivityMainBinding binding;
 
-		setContentView(R.layout.activity_main);
+	@Override
+	protected void onCreate(Bundle state) {
+		super.onCreate(state);
+
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		binding.setPresenter(this);
+	}
+
+	public void onCreateClick(View view) {
+		Toast.makeText(
+			MainActivity.this, "Click do add pool", Toast.LENGTH_SHORT).show();
 	}
 
 }
