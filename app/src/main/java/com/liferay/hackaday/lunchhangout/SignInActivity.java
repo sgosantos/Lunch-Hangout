6dd7016d26a20387285a32635a38f13eb7c277e0
Silvio Santos <silvio.santos@liferay.com>
2016-08-19 15:54:00 -0300

    Use sign in call

--- app/src/main/java/com/liferay/hackaday/lunchhangout/SignInActivity.java
+++ app/src/main/java/com/liferay/hackaday/lunchhangout/SignInActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.liferay.hackaday.lunchhangout.databinding.ActivitySignInBinding;
+import com.liferay.hackaday.lunchhangout.model.AuthToken;
+import com.liferay.hackaday.lunchhangout.service.LunchHangoutService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -21,8 +26,34 @@ public class SignInActivity extends AppCompatActivity {
 	}
 
 	public void loginClick(View view) {
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
+		Call<AuthToken> call = LunchHangoutService.getInstance().signIn(
+			binding.username.getText().toString(),
+			binding.password.getText().toString(), "password");
+
+		call.enqueue(new Callback<AuthToken>() {
+			@Override
+			public void onResponse(
+				Call<AuthToken> call, Response<AuthToken> response) {
+
+				if (response.isSuccessful()) {
+					Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+					startActivity(intent);
+					finish();
+				}
+				else {
+					Toast.makeText(
+						SignInActivity.this, "Invalid login or password",
+						Toast.LENGTH_SHORT).show();
+				}
+			}
+
+			@Override
+			public void onFailure(Call<AuthToken> call, Throwable t) {
+				Toast.makeText(
+					SignInActivity.this, "Connection error",
+					Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
 	public void signUpClick(View view) {
